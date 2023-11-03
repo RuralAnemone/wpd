4e9cf0824adc7bd36a9dc9fd59f7a3fce4e0b148
Isaiah W <ruralanemone@gmail.com>
2023-11-03 13:24:55 -0500

    add theme toggle button

---
+++ html/3/7/script.js
@@ -0,0 +1,32 @@
+let root = document.querySelector(':root');
+
+// document.body already has class="dark" on it
+
+if (localStorage.getItem('theme') === null) {
+	localStorage.setItem('theme', 'dark');
+}
+
+if (localStorage.getItem('theme') === 'light') toLight();
+	else toDark();
+
+function toLight() {
+	localStorage.setItem('theme', 'light');
+	document.body.classList.remove('dark');
+	document.querySelector('div.theme-toggle > button > img').setAttribute('src', 'img/light.svg');
+}
+
+function toDark() {
+	localStorage.setItem('theme', 'dark');
+	document.body.classList.add('dark');
+	document.querySelector('div.theme-toggle > button > img').setAttribute('src', 'img/dark.svg');
+}
+
+function toggleTheme() {
+	console.log(`switching theme from ${localStorage.getItem('theme')}...`);
+	if (localStorage.getItem('theme') === 'dark') {
+		toLight();
+	} else {
+		toDark();
+	}
+	console.log(`...to ${localStorage.getItem('theme')}`);
+}
