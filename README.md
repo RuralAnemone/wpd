de39c5203a6b8e83f39ac025b2026d18d5bcae61
Isaiah W <ruralanemone@gmail.com>
2023-11-26 16:26:24 -0600

    format everything (:

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
 # wpd
+
 web page design files (class)
 
 ## Sitemap:
-- html
-	- 2
-		- [4](html/2/4)
-		- [6](html/2/6)
-		- [performance](html/2/performance)
 
-	- 3
-		- [7](html/3/7)
-		- [performance](html/3/performance/)
+-   html
+
+    -   2
+
+        -   [4](html/2/4)
+        -   [6](html/2/6)
+        -   [performance](html/2/performance)
+
+    -   3
+        -   [7](html/3/7)
+        -   [performance](html/3/performance/)
--- html/3/performance/theme-toggle.js
+++ html/3/performance/theme-toggle.js
@@ -1,30 +1,30 @@
 // document.body already has class="dark" on it
 
-if (localStorage.getItem('theme') === null) {
-	localStorage.setItem('theme', 'dark');
+if (localStorage.getItem("theme") === null) {
+	localStorage.setItem("theme", "dark");
 }
 
-if (localStorage.getItem('theme') === 'light') toLight();
-	else toDark();
+if (localStorage.getItem("theme") === "light") toLight();
+else toDark();
 
 function toLight() {
-	localStorage.setItem('theme', 'light');
-	document.body.classList.remove('dark');
-	document.querySelector('div.theme-toggle > button > img').setAttribute('src', 'img/light.svg');
+	localStorage.setItem("theme", "light");
+	document.body.classList.remove("dark");
+	document.querySelector("div.theme-toggle > button > img").setAttribute("src", "img/light.svg");
 }
 
 function toDark() {
-	localStorage.setItem('theme', 'dark');
-	document.body.classList.add('dark');
-	document.querySelector('div.theme-toggle > button > img').setAttribute('src', 'img/dark.svg');
+	localStorage.setItem("theme", "dark");
+	document.body.classList.add("dark");
+	document.querySelector("div.theme-toggle > button > img").setAttribute("src", "img/dark.svg");
 }
 
 function toggleTheme() {
-	console.log(`switching theme from ${localStorage.getItem('theme')}...`);
-	if (localStorage.getItem('theme') === 'dark') {
+	console.log(`switching theme from ${localStorage.getItem("theme")}...`);
+	if (localStorage.getItem("theme") === "dark") {
 		toLight();
 	} else {
 		toDark();
 	}
-	console.log(`...to ${localStorage.getItem('theme')}`);
+	console.log(`...to ${localStorage.getItem("theme")}`);
 }
