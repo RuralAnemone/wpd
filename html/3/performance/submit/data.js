13ff48b12c6e153f83d1f9667a9c64a6e7c52891
Isaiah W <64103005+RuralAnemone@users.noreply.github.com>
2023-12-05 10:57:53 -0600

    fix the form submission, render data

---
+++ html/3/performance/submit/data.js
@@ -0,0 +1,15 @@
+const dataArea = document.querySelector("div.data>pre");
+
+const usp = new URLSearchParams(location.search);
+
+let data = {};
+
+for (const [key, value] of usp.entries()) data[key] = value;
+
+dataArea.innerText = JSON.stringify(data)
+					.replaceAll(',', ',\n	')
+					.replace('{','{\n	')
+					.replace('}','\n}')
+					.replaceAll(':',': ');
+
+// I mean I could use prettier but I don't really feel like it eh
