bd07450238c4167ade2861f2a3fbb02965f67af7
Isaiah W <ruralanemone@gmail.com>
2024-01-23 03:22:49 -0600

    make le new pages!

--- FINAL/fun-stuff.js
+++ FINAL/fun-stuff.js
@@ -1,3 +1,11 @@
-document.querySelector('#a-second').addEventListener('click', () => {
+const jsConfetti = new JSConfetti();
+
+document.querySelector('#a-second')?.addEventListener('click', () => {
 	timer(60 * 1000);
+});
+
+document.querySelector('#linux')?.addEventListener('mouseover', () => {
+	jsConfetti.addConfetti({
+		emojis: ['🐧']
+	});
 });
